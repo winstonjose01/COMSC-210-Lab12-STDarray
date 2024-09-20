4d3511b3e1a7ef97c5eba018b25d2d4eac9ad801
winstonjose01 <winston.jose@gmail.com>
2024-09-20 15:27:06 -0700

    revision 1: add fstream fns

---
+++ lab12_stdarray.cpp
@@ -0,0 +1,36 @@
+# include <iostream>
+# include <array>
+# include <algorithm>
+# include <numeric>
+# include <fstream>
+using namespace std;
+
+
+int openFile(fstream &, string);
+void closeFile (fstream &);
+
+int main(){
+
+fstream budgetfile;
+string filename = "budget.txt";
+int fstat;
+fstat = openFile(budgetfile,filename);
+if (fstat < 0){
+    exit(-1);
+}
+
+return 0;
+
+
+}
+
+int openFile(fstream &f, string fname){
+    f.open(fname.data(), ios::in);
+    if(!f){
+        cout << "Error opening file. Program aborting\n";
+        return -1;
+    }
+    else{
+        return 1;    
+    }
+}

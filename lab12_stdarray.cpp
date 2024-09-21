ec819b26326ad0b282ad4cb93ea325460741e1a5
winstonjose01 <winston.jose@gmail.com>
2024-09-21 10:29:33 -0700

    revision-3: using iterators, max ,min, sum

--- lab12_stdarray.cpp
+++ lab12_stdarray.cpp
@@ -10,16 +10,20 @@
 
 using namespace std;
 
-const int SIZE = 30;
-
-//Function declaration
-int openFile(fstream &, string);
-void closeFile (fstream &);
+const int SIZE = 30; // Constant size for the
 
 struct GroceryItem{
     string item;
     double prices;
 };
+
+//Function declaration
+int openFile(fstream &, string);
+void closeFile (fstream &);
+bool sortbyItem (GroceryItem &, GroceryItem &);
+bool sortbyPrice (GroceryItem &, GroceryItem &);
+
+
 
 int main(){
 
@@ -61,16 +65,49 @@ int main(){
      cout << "----------------------------------------------------\n";
     
     // Accessing individual elements: 
-    cout << "\nElement 11 item and price: "<< list.at(11).item << " | $" << list.at(11).prices;
-    cout << "\nElement 29 item and price: "<< list[29].item << " | $" << list[29].prices;
-    cout << "\nFront item and price: "<< list.front().item<< " | $" << list.front().prices;
-    cout << "\nBack item and price: "<< list.back().item<< " | $" << list.back().prices;
-    
+    cout << "\n3. Element 11 item and price: "<< list.at(11).item << " | $" << list.at(11).prices;
+    cout << "\n4. Element 29 item and price: "<< list[29].item << " | $" << list[29].prices;
+    cout << "\n5. Front item and price: "<< list.front().item<< " | $" << list.front().prices;
+    cout << "\n6. Back item and price: "<< list.back().item<< " | $" << list.back().prices;
+    cout << "\n7. Empty ? " << (list.empty() == 0? "False" : "True");
+    cout << "\n8. Address? " << list.data();
+    cout << "----------------------------------------------------\n";
 
+    // Using iterators to sort
+    sort(list.begin(), list.end(), sortbyItem); // Sort by item using a comparator function
+    cout << "\n9.  Sorted by item :\n";
+    for (GroceryItem val :  list) cout << "\t" << val.item << " | $" << val.prices << endl;
+    cout << "----------------------------------------------------\n";
+    sort(list.begin(), list.end(), sortbyPrice); // Sort by price using a comparator function
+    cout << "\n10.  Sorted by price :\n";
+    for (GroceryItem val :  list) cout << "\t" << val.item << " | $" << val.prices << endl;
+    cout << "----------------------------------------------------\n";
+    sort(list.begin(), list.end(), sortbyItem); // Sort by item using a comparator function
+    cout << "\n11.  Reverse sorting by item :\n";
+    sort(list.rbegin(), list.rend(), sortbyItem);
+    for (GroceryItem val :  list) cout << "\t" << val.item << " | $" << val.prices << endl;
+    cout << "----------------------------------------------------\n";
+
+    // find max & min & sum
+    cout << "12. Max: $" << max_element(list.begin(), list.end(),[](GroceryItem &a, GroceryItem &b)
+         {return a.prices < b.prices;})->prices;
+    cout << "\n13. Min: $" << max_element(list.begin(), list.end(),[](GroceryItem &a, GroceryItem &b)
+         {return a.prices > b.prices;})->prices;
+    cout << "\n13. Sum: $" << accumulate(list.begin(), list.end(),0.0,[](double sum, GroceryItem &b)
+         {return sum + b.prices;});
+
+    budgetfile.close();
 
     return 0;
 
+}
 
+bool sortbyItem(GroceryItem &a, GroceryItem&b){
+    return (a.item < b.item);
+}
+
+bool sortbyPrice(GroceryItem &a, GroceryItem&b){
+    return (a.prices < b.prices);
 }
 
 int openFile(fstream &f, string fname){

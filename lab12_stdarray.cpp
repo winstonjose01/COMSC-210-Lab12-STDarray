f1197dcf5b247331b6a5bdc5602264930ce8cffd
winstonjose01 <winston.jose@gmail.com>
2024-09-20 23:08:57 -0700

    revision - 1: array of struct and reading file

--- lab12_stdarray.cpp
+++ lab12_stdarray.cpp
@@ -5,23 +5,66 @@
 # include <algorithm>
 # include <numeric>
 # include <fstream>
+# include <sstream>
+# include <string>
+
 using namespace std;
 
+const int SIZE = 30;
 
+//Function declaration
 int openFile(fstream &, string);
 void closeFile (fstream &);
 
+struct GroceryItem{
+    string item;
+    double prices;
+};
+
 int main(){
 
-fstream budgetfile;
-string filename = "budget.txt";
-int fstat;
-fstat = openFile(budgetfile,filename);
-if (fstat < 0){
-    exit(-1);
-}
+    fstream budgetfile;
+    string filename = "budget.csv";
+    int fstat;
+    string line;
+    array<GroceryItem, SIZE> list; // Array of structs
+ 
 
-return 0;
+    fstat = openFile(budgetfile,filename);
+    if (fstat < 0){
+        exit(-1);
+    }
+    int i = 0;
+    while (getline(budgetfile, line) && i < SIZE){
+        stringstream ss(line);
+        string priceStr;
+
+        getline(ss, list[i].item,',');
+        getline(ss,priceStr);//{
+        if (!priceStr.empty())
+            try{
+            list[i].prices = stod(priceStr);
+            }
+            catch(const invalid_argument &e){
+            cout << "Invalid price " << priceStr << endl;
+            }
+        i++;
+    }
+
+    // Report the size of each array
+    cout << "1. Size of Grocery list: " << list.size() << endl;
+    cout << "----------------------------------------------------\n";
+
+    // Values of array of struct 
+    cout << "2. Accessing value of the Grocery list:\n";
+    for (GroceryItem val :  list) cout << "\t" << val.item << " | $" << val.prices << endl;
+     cout << "----------------------------------------------------\n";
+    
+    // Accessing individual elements: 
+
+
+
+    return 0;
 
 
 }
@@ -30,7 +73,7 @@ int openFile(fstream &f, string fname){
     f.open(fname.data(), ios::in);
     if(!f){
         cout << "Error opening file. Program aborting\n";
-        return -1;
+        return 0;
     }
     else{
         return 1;    
